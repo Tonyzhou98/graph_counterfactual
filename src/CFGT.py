8355d74f790fa2504d0e8c05e3797e0129865324
ZYH <31127200+Tonyzhou98@users.noreply.github.com>
2022-12-01 22:58:56 -0500

    Update CFGT.py

--- src/CFGT.py
+++ src/CFGT.py
@@ -64,7 +64,7 @@ class CFGT(nn.Module):
             idx_1 = adj.to_dense().reshape(-1) == 1
             weight[idx_1] = weights_1
 
-            loss_bce = nn.BCELoss(weight=weight, reduction='mean')
+            loss_bce = nn.BCELoss(weight=weight, reduction='mean').to('cpu')
             loss_reconst_a = loss_bce(A_pred.reshape(-1).cpu(), adj.to_dense().reshape(-1).cpu())
         else:
             loss_bce = nn.BCELoss(reduction='mean')
