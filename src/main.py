2f9e2f105385c293461492acac727e585f157a4b
ZYH <31127200+Tonyzhou98@users.noreply.github.com>
2022-12-02 14:29:10 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -17,7 +17,6 @@ from sklearn.linear_model import LinearRegression
 from sklearn.manifold import TSNE as tsn
 
 import torch
-torch.cuda.empty_cache()
 import torch.nn.functional as F
 import torch.optim as optim
 import matplotlib.pyplot as plt
