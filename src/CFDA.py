c77ed35d727a61c590a6433dfee594b37b0d5fc2
ZYH <31127200+Tonyzhou98@users.noreply.github.com>
2023-02-07 21:34:22 -0500

    Update CFDA.py

--- src/CFDA.py
+++ src/CFDA.py
@@ -52,12 +52,6 @@ class CFDA(nn.Module):
             # sampled_z = gaussian_noise * torch.exp(logstd) + mean
             std = torch.exp(0.1 * logstd)
             sampled_z = gaussian_noise * std + mean
-
-            print(mean)
-            print(sampled_z)
-            print(logstd)
-            print(torch.any(torch.isnan(std)).item())
-            print(torch.any(torch.isinf(std)).item())
         else:
             sampled_z = mean
         return sampled_z
