0c278e6e7c297bfc99d0a6826ae2a47d7c89b05c
ZYH <31127200+Tonyzhou98@users.noreply.github.com>
2023-02-07 22:10:39 -0500

    Update CFDA.py

--- src/CFDA.py
+++ src/CFDA.py
@@ -50,7 +50,7 @@ class CFDA(nn.Module):
         gaussian_noise = torch.randn_like(mean, requires_grad=True)
         if self.training and self.type == 'VGAE':
             # sampled_z = gaussian_noise * torch.exp(logstd) + mean
-            std = torch.exp(0.1 * logstd)
+            std = torch.exp(0.05 * logstd)
             sampled_z = gaussian_noise * std + mean
         else:
             sampled_z = mean
